f2b782a0e91e2ed42820df5f20f0c025bf4c47fa
danielfgdev <danielfgdev@gmail.com>
2024-07-03 01:10:01 +0200

    fix

--- ACTIVIDADES/actividad 1-11/script.js
+++ ACTIVIDADES/actividad 1-11/script.js
@@ -1,25 +1,5 @@
 let tecla = 'a'
 
-// if (tecla === 'a') {
-//     console.log("Se mueve a la izquierda");
-// }
-
-// if (tecla === 'd') {
-//     console.log("Se mueve a la derecha");
-// }
-
-// if (tecla === 'w') {
-//     console.log("Se mueve hacia delante");
-// }
-
-// if (tecla === 's') {
-//     console.log("Se mueve hacia atras");
-// }
-
-// else {
-//     console.log("Esa tecla no hace nada");
-// }
-
 if (tecla === 'a') {
     console.log("Se mueve a la izquierda");
 } else if (tecla === 'd') {
@@ -31,6 +11,3 @@ if (tecla === 'a') {
 } else {
     console.log("No hace nada");
 }
-
-
-
