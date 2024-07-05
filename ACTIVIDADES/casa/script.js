7d032d2b7526aa4f190ec9595df87837f80286d4
danielfgdev <danielfgdev@gmail.com>
2024-07-05 03:41:38 +0200

    nuevos ejercicios

---
+++ ACTIVIDADES/casa/script.js
@@ -0,0 +1,20 @@
+// ! Contar de 10 hacia alante
+// for (let i = 0; i <= 10; i = i + 1) {
+//     console.log(i);
+// }
+
+// ! Contar de 10 hacia atras
+// for (let i = 10; i >= 0; i = i - 1) {
+//     console.log(i);
+// }
+
+// ! Contar hasta 100 mostrando solo los numeros pares
+// for (let i = 0; i <= 100; i = i + 2) {
+//     console.log(i);
+// }
+
+// ! Contar hasta 100 mostrando solo los numeros impares
+// for (let i=1;i<100; i=i+2){
+//     console.log(i);
+// }
+
