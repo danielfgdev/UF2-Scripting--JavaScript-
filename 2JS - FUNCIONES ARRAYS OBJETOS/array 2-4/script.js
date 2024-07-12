40a53f134a8b46d55ca13092b781084789e9bd0e
danielfgdev <danielfgdev@gmail.com>
2024-07-12 20:15:41 +0200

    nuevos ejercicios

---
+++ 2JS - FUNCIONES ARRAYS OBJETOS/array 2-4/script.js
@@ -0,0 +1,12 @@
+const max = 5
+const min = 227
+let randomNumbers = []
+
+for (let i = 0; i < 10; i = i + 1) {
+    let random = Math.random() * (max - min + 1) + min
+    let randomInt = Number(random.toFixed(0))
+    console.log(random.toFixed(0))
+    randomNumbers.push(randomInt)
+}
+console.log(randomNumbers)
+console.log(randomNumbers[6])
