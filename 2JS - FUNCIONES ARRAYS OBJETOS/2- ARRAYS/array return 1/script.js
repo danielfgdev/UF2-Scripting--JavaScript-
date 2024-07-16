b75fca00c301c0693f8bacaae627c1fc1d531c47
danielfgdev <danielfgdev@gmail.com>
2024-07-16 20:25:21 +0200

    nuevos ejercicios

---
+++ 2JS - FUNCIONES ARRAYS OBJETOS/2- ARRAYS/array return 1/script.js
@@ -0,0 +1,25 @@
+// ! Funcion que recibe un numero y devuelve el doble.
+
+// function doble(a) {
+//     return a * 2
+
+// }
+// doble(3)
+
+// let variable = doble(3)
+// console.log(variable)
+// console.log(doble(3))
+
+// ! Funcion que devuelve true o false si un numero es mayor de edad.
+
+function edad(años) {
+    if (años >= 18) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+edad(15)
+console.log(edad(18))
---
+++ 2JS - FUNCIONES ARRAYS OBJETOS/2- ARRAYS/array return 2/script.js
@@ -0,0 +1,10 @@
+const dani = {
+    nombre: "Dani",
+    apellido: "Fernandez",
+    edad: 35
+}
+
+function edad(persona) {
+    console.log(persona.edad)
+}
+edad(dani)
